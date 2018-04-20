b5a752c1d72634b8e013768c90d0f69095b52585
mtking78 <mtking78@gmail.com>
2018-04-19 18:31:13 -0700

    Routes working.

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+// Friends List (DATA)
+// =============================================================
+var friends = [
+    {
+        name: "John Doe",
+        photo: "placeholder",
+        scores: [1,2,3,4,5,1,2,3,4,5]
+    }
+]
+
+module.exports = friends;
