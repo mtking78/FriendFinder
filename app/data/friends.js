99474b5b9809c19f7de07c4ca3d05de145e4dd70
mtking78 <mtking78@gmail.com>
2018-04-23 10:21:30 -0700

    css issues

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,27 +4,32 @@ var friends = [
     {
         name: "Bert",
         photo: "https://vignette.wikia.nocookie.net/muppet/images/2/24/Bert_season_6.jpeg/revision/latest/scale-to-width-down/250?cb=20170417204846",
-        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+        quote: "Looking for someone without rubber ducks.",
+        scores: ["5", "5", "5", "1", "4", "5", "1", "5", "1", "1"]
     },
     {
         name: "Cookie Monster",
         photo: "https://vignette.wikia.nocookie.net/muppet/images/f/f2/Cookie84.jpg/revision/latest/scale-to-width-down/250?cb=20150809025439",
-        scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+        quote: "HUNGRY",
+        scores: ["2", "3", "2", "5", "1", "2", "3", "1", "4", "5"]
     },
     {
         name: "Oscar the Grouch",
         photo: "https://vignette.wikia.nocookie.net/muppet/images/e/e0/UntitledO.png/revision/latest/scale-to-width-down/250?cb=20130812000527",
-        scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+        quote: "I want someone who is trashy.",
+        scores: ["4", "4", "5", "5", "2", "3", "4", "3", "5", "2"]
     },
     {
         name: "Fozzie Bear",
         photo: "https://vignette.wikia.nocookie.net/muppet/images/2/2c/KidFozzie.jpg/revision/latest/scale-to-width-down/250?cb=20060623222000",
-        scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+        quote: "I prefer my mate to be unshaven.",
+        scores: ["1", "5", "2", "4", "3", "3", "4", "2", "3", "4"]
     },
     {
         name: "Gonzo",
         photo: "https://vignette.wikia.nocookie.net/muppet/images/3/30/Gonzo-OneEyelid.jpg/revision/latest/scale-to-width-down/250?cb=20111121211149",
-        scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+        quote: "I can please you in unusual ways.",
+        scores: ["3", "2", "3", "2", "2", "5", "1", "3", "3", "1"]
     }
 ]
 
