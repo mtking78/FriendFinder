10015d9c2868e71f5fd11b7dd70164bdd2d247ba
mtking78 <mtking78@gmail.com>
2018-04-22 23:05:23 -0700

    Best match calculated, modal displays result.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,9 +2,29 @@
 // =============================================================
 var friends = [
     {
-        name: "John Doe",
-        photo: "placeholder",
-        scores: [1,2,3,4,5,1,2,3,4,5]
+        name: "Bert",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/2/24/Bert_season_6.jpeg/revision/latest/scale-to-width-down/250?cb=20170417204846",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        name: "Cookie Monster",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/f/f2/Cookie84.jpg/revision/latest/scale-to-width-down/250?cb=20150809025439",
+        scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+    },
+    {
+        name: "Oscar the Grouch",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/e/e0/UntitledO.png/revision/latest/scale-to-width-down/250?cb=20130812000527",
+        scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    },
+    {
+        name: "Fozzie Bear",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/2/2c/KidFozzie.jpg/revision/latest/scale-to-width-down/250?cb=20060623222000",
+        scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+    },
+    {
+        name: "Gonzo",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/3/30/Gonzo-OneEyelid.jpg/revision/latest/scale-to-width-down/250?cb=20111121211149",
+        scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     }
 ]
 
