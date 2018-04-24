7698f6dea7dbb03c72fbf461a39b76e94cda5ee1
mtking78 <mtking78@gmail.com>
2018-04-23 21:47:33 -0700

    Images added to readme

--- app/data/friends.js
+++ app/data/friends.js
@@ -30,6 +30,24 @@ var friends = [
         photo: "https://vignette.wikia.nocookie.net/muppet/images/3/30/Gonzo-OneEyelid.jpg/revision/latest/scale-to-width-down/250?cb=20111121211149",
         quote: "I can please you in unusual ways.",
         scores: ["3", "2", "3", "2", "2", "5", "1", "3", "3", "1"]
+    },
+    {
+        name: "Majory the Trash Heap",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/b/b8/Trash_heap.jpg/revision/latest/scale-to-width-down/280?cb=20101120181311",
+        quote: "Let's get dirty!",
+        scores: ["4", "4", "4", "5", "1", "4", "4", "2", "2", "2"]
+    },
+    {
+        name: "Kermit",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/7/79/Kermit-the-frog.jpg/revision/latest/scale-to-width-down/280?cb=20101015153557",
+        quote: "Have you heard about what a frog can do with its tongue?",
+        scores: ["1", "3", "1", "3", "2", "4", "3", "5", "2", "4"]
+    },
+    {
+        name: "Janice",
+        photo: "https://vignette.wikia.nocookie.net/muppet/images/e/e5/Janice_m15.jpg/revision/latest/scale-to-width-down/280?cb=20151008230827",
+        quote: "Let's get naked!",
+        scores: ["4", "1", "2", "5", "3", "2", "4", "1", "5", "4"]
     }
 ]
 
